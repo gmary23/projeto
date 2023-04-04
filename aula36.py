ab8a6a94287593387e958ea71abe159357edf761
gmary23 <geila.estudos@gmail.com>
2023-04-04 13:45:10 -0300

    aula de operadores de atribuição

---
+++ aula36.py
@@ -0,0 +1,17 @@
+"""
+Operadores de atribuição
+= igual
++=  adição
+-= subtração
+*= multiplicação
+/= divisão
+//= divisão inteira
+**= potenciação
+ %= módulo / verifica se divide por 2 == 0 (número par)
+"""
+contador = 2
+
+###
+
+contador *= 100
+print(contador)
