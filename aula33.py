c5b8e5ccdb6b371a3a1daf833bf60b56ffbb909c
gmary23 <geila.estudos@gmail.com>
2023-04-04 13:33:43 -0300

    subindo aulas 33, 34 e 35

---
+++ aula33.py
@@ -0,0 +1,10 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'geila Maria'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(outra_variavel)
+print(string.zfill(50)) #preenche espaços vazios.
+print(string.capitalize()) #Trasforma a primeira letra em maiúscula.
