d328c06d06348e95735c759668c2b20329314018
gmary23 <geila.estudos@gmail.com>
2023-04-12 12:08:44 -0300

    enviando calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,50 @@
-num_1 = int(input('Digite o primeiro número: '))
-num_2 = int(input('Digite o segundo número: '))
+while True:
+# pede para digitar os números e o operador
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite um operador: ')
+#uma flag que diz que os numero são 
+    numeros_validos = None
 
-print('{} + {} = '.format (num_1, num_2))
-print(num_1 + num_2)
+    
+    try:
+        num_1 = float(numero_1)
+        num_2 = float(numero_2)
+        numeros_validos = True
+        
+    except:
+        numeros_validos = None
 
-print('{} - {} ='.format (num_1, num_2))
-print(num_1 - num_2)
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados são inválidos')
+        continue
+
+#coloca em uma variável os operadores permitidos 
+    operadores_permitidos = '+-/*'
+# verifica se o operador digitado é o permitido
+    if operador not in operadores_permitidos:
+        print('Operador inválido')
+        continue
+# checa se digitou apenas um operador
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+# a partir desse ponto que inicia a calculadora em si
+    print('Realizando sua conta confira o resultado abaixo.')
+    if operador =='+':
+        print(num_1 + num_2) 
+    if operador =='-':
+        print(num_1 - num_2) 
+    if operador =='/':
+        print(num_1 + num_2) 
+    if operador =='*':
+        print(num_1 + num_2) 
+  # aqui fala que se quiser sair basta digitar s  
+    sair = input('Quer sair?[s]im: ').lower().startswith('s')
+
+    if sair is True:
+        break
+
+
+
+
