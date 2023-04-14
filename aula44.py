13527da524607cdec131cad9d381c45e07670dab
geilamaria <sc.geilamartins@gmail.com>
2023-04-14 08:12:45 -0300

    Iniciando for + range

---
+++ aula44.py
@@ -0,0 +1,10 @@
+'''
+For + Range
+range -> range (start, stop, step(o step pula conforme for solicitado, no exemplo abaixo pula de 2 em 2))
+na opção FOR não precisa criar variavel com indice
+'''
+numeros = range(0, 100, 2)
+
+for numero in numeros:
+    print(numero)
+
