2d99cd19065a62bf594aae12e8444cb1b2ad6e3b
gmary23 <geila.estudos@gmail.com>
2023-04-19 07:52:05 -0300

    Introdução a empacomento e desempacotamento

---
+++ aula51_introducao_empacotamento.py
@@ -0,0 +1,6 @@
+'''
+Introdução ao empacotamento e desempacotamento
+'''
+
+nome1, nome2, *resto = ['Maria', 'Helena', 'Luiz', 'João'] # qdo não tem variável suficiente cria outra com um asterisco antes que quer dizer que pega todos os elementos
+print(nome1, nome2, resto) 
