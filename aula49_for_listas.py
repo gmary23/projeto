ce1554207e528b82c70d2b9dfa50d15dc2dd5f67
gmary23 <geila.estudos@gmail.com>
2023-04-19 07:44:41 -0300

    for in com listas

---
+++ aula49_for_listas.py
@@ -0,0 +1,14 @@
+'''
+for in com listas
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+'''
+
+lista = ['Maria', 'Helena', 'Luiz']
+indices = range(len(lista))
+
+for bola in indices:
+    print(bola,lista[bola])
