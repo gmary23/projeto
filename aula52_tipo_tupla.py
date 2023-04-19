31b37514fd9257150b0a0017fa9fdae5c5e313a9
gmary23 <geila.estudos@gmail.com>
2023-04-19 08:11:47 -0300

    Inicio de Tuplas

---
+++ aula52_tipo_tupla.py
@@ -0,0 +1,8 @@
+'''
+Tipo tupla - Uma lista imutável
+'''
+nomes = 'Maria', 'Helena', 'Luiz'
+#nomes = list(nomes) #- esse comando transforma a tupla em lista
+# nomes = tupla(nomes)
+print(nomes[-1])
+print(nomes)
