ed7f31bc07dbfa6811f14a0bdc16e65518f3c39e
geilamaria <sc.geilamartins@gmail.com>
2023-05-11 14:09:54 -0300

    Aula 70 retorno de valores

---
+++ aula65.py
@@ -0,0 +1,10 @@
+# por padrão função python retorna (None)
+
+def imprimir (a, b, c): # o que está entre parenteses é PARAMETROS - a variável 
+    print(a, b, c)
+    #print('Várias2')
+    #print('Várias3')
+    #print('Várias4')
+
+imprimir(1, 2 , 3) # o que tem dentro dos parenteses são os VALORES
+imprimir (4, 5, 6) # O legal da função é que a cada fez que é chamada essa função é possível mudar os parâmetros
---
+++ aula70.py
@@ -0,0 +1,16 @@
+'''
+Retorno de valores das funções (return)
+'''
+
+def soma(x, y):
+    if x > 10: # aqui identificou que na linha 14 o valor de x é 11...então ele retorna o valor abaixo que é 10 e 20.
+        return[ 10, 20 ] # retorna esse valor por o x é maior que 10.
+    return x + y
+
+
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+
+print(soma(11, 55))
+print(soma1) 
+print(soma2)
