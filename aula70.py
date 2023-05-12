01a82c2786f3bffa86f712aefc727cbd75700f9b
gmary23 <geila.estudos@gmail.com>
2023-05-12 11:07:01 -0300

    modificação da aula 70

--- aula70.py
+++ aula70.py
@@ -27,9 +27,9 @@ Retorno de valores das funções (return)
 #    return x + y
 
 
-soma1 = soma(2, 2)
-soma2 = soma(3, 3)
+# soma1 = soma(2, 2)
+# soma2 = soma(3, 3)
 
-print(soma(11, 55))
-print(soma1)
-print(soma2)
+# print(soma(11, 55))
+# print(soma1)
+# print(soma2)
