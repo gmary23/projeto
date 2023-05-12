d2ca9958049b43e49ffec3565b188f6386b4d184
gmary23 <geila.estudos@gmail.com>
2023-05-12 10:35:15 -0300

    Retorno de função

---
+++ aula70.py
@@ -0,0 +1,16 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+
+def soma(x, y):
+    print("Olha")
+    print("so")
+    print("que")
+    print("legal")
+    return x - y
+
+
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+print(soma1 + soma2)
